4b3810aac79744c6c269c91514ef51b7daddd225
adamlinhandsome <43054361+adamlinhandsome@users.noreply.github.com>
2024-02-06 11:51:54 +0800

    Create index.js

---
+++ static/js/index.js
@@ -0,0 +1,27 @@
+exports.aceGetFilterStack = function(name, context){
+  return [
+    context.linestylefilter.getRegexpFilter(
+      new RegExp("<br/><table(.*?)table><br/>", "g"), 'table')
+  ];
+}
+
+
+exports.aceCreateDomLine = function(name, args){
+  if (args.cls.indexOf('table') > -1) { // If it's a table
+    var content;
+    cls = args.cls.replace(/(^| )table:(\S+)/g, function(x0, space, tableContent) {
+      content = tableContent;
+      return space + "table table_" + tableContent;
+    });
+    //cls=args.cls;
+
+    return [{
+      cls: cls,
+      //extraOpenTags: '<span style="display:block;"><table style=" border: 2px solid rgb(140 140 140);  letter-spacing: 1px;text-align:center;">' +cls+content + '</table></span>',
+      extraOpenTags: '<div style="display:block;">'+content + '</div>',
+      extraCloseTags:''
+    }];
+  }
+}
+
+
